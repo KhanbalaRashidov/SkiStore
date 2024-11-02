c7c9b161cf3fbc79867c081715fafbfcb0c310ab
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-11-02 20:07:00 +0400

    edit

--- OnionArchitecture/Entities/Models/Product.cs
+++ OnionArchitecture/Entities/Models/Product.cs
@@ -13,9 +13,7 @@ namespace Entities.Models
         public decimal Price { get; set; }
         public required string PictureUrl { get; set; }
         public required string  Type { get; set; }
-        public required string Type { get; set; }
         public required string Brand { get; set; }
-        public int QuantitiyInStock { get; set; }
         public int QuantityInStock { get; set; }
     }
 }
